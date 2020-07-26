8f85bc78c1ddb2d3ec76f2b7a75f26328c1178d9
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-26 15:43:48 +0100

    added delete button to HTML and added eventlistener on jS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
+    console.log('JavaScript loaded');
+
+    const submitForm = document.querySelector('#animal-list');
+    submitForm.addEventListener('submit', handleFormSubmit);
+
+    const deleteForm = document.querySelector('#delete-form');
+    deleteForm.addEventListener('submit', handleFormDelete);
+});
