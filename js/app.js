893bc7719775911b78035596f77447c39ab994e8
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-26 16:35:05 +0100

    created and edited handleFormSubmit function

--- js/app.js
+++ js/app.js
@@ -7,3 +7,31 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteForm = document.querySelector('#delete-form');
     deleteForm.addEventListener('submit', handleFormDelete);
 });
+
+const handleFormSubmit = function (event) {
+    event.preventDefault();
+
+    const animal = document.createElement('li');
+    animal.textContent = `${event.target.animal.value}`;
+    animal.classList.add('animal-item');
+
+    const species = document.createElement('li');
+    species.textContent = `${event.target.species.value}`;
+    species.classList.add('species-item');
+
+    const continent = document.createElement('li');
+    continent.textContent = `${event.target.continent.value}`;
+    continent.classList.add('continent-item');
+
+    const conservationStatus = document.createElement('li');
+    conservationStatus.textContent = `${event.target.conservation-status.value}`;
+    conservationStatus.classList.add('conservationStatus-item');
+
+
+};
+
+
+const handleFormDelete = function (event) {
+
+
+};
