5d4f52729f564b3d75d78d451cb4c79e9a336672
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-07-26 22:41:48 +0100

    ADDED CSS

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#new-form');
     form.addEventListener('submit', handleFormSubmit);
 
-    const deleteForm = document.querySelector('#delete-form');
-    deleteForm.addEventListener('submit', handleFormDelete);
+    const deleteButton = document.querySelector('#delete-all');
+    deleteButton.addEventListener('click', handleDeleteButton);
 });
 
 const handleFormSubmit = function(event) {
@@ -14,19 +14,19 @@ const handleFormSubmit = function(event) {
     const animal = event.target.animal.value;
     const species = event.target.species.value;
     const continent = event.target.continent.value;
-    const conservationStatus = event.target.conservation-status.value;
+    const status = event.target.status.value;
 
     const animalList = document.querySelector('#animal-list');
     const newItem = document.createElement('li');
     newItem.classList.add('animal-item');
-    newItem.textContent = `Animal: ${animal} | Species: ${species} | Continent: ${continent} | Conservation Status: ${conservationStatus}`
+    newItem.textContent = `Animal: ${animal} | Species: ${species} | Continent: ${continent} | Conservation Status: ${status}`
 
     animalList.appendChild(newItem);
 
     event.target.reset();
 };
 
-const handleFormDelete = function() {
+const handleDeleteButton = function() {
     const deletedList = document.querySelector('#animal-list');
     deletedList.textContent = '';
 };
